cbc6d3d643e1b65f6a1426f3f476167cefe27eca
Natã Félix <Nata.analista@gmail.com>
2025-01-29 22:49:23 -0300

    Concluído as funcionalidades básicas do desafio

--- app.js
+++ app.js
@@ -1,30 +1,44 @@
-listaAmigos = [];
-console.log(listaAmigos);
+let listaAmigos = [];
+let listaDeSorteados = [];
+
+function mostrarLista() {
+  document.getElementById("listaAmigos").innerHTML = listaAmigos;
+}
 
 function adicionarAmigo() {
-  let amigoInput = document.getElementById("amigo").value;
-  let amigo = amigoInput.split(",");
-  console.log(amigo);
-
+  amigo = document.getElementById("amigo").value;
   if (amigo == "") {
-    alert("Por favor, insira um nome");
-    console.log(amigo);
-    return;
-  } else if (listaAmigos.includes(amigo)) {
-    alert(`O nome ${amigo} já foi adicionado`);
-    console.log(amigo);
-    return;
+    alert("Digite um nome válido");
+  } else {
+    listaAmigos.push(amigo);
+    mostrarLista();
   }
-
-  listaAmigos.push(amigo);
-  document.getElementById("amigo").value = "";
-  console.log(amigo);
 }
 
 function sortearAmigo() {
   if (listaAmigos.length == 0) {
-    alert("Por favor,insira um nome antes de sortear");
-    return;
+    alert("Adicione um amigo antes de sortear");
+  } else {
+    let sorteado = listaAmigos[Math.floor(Math.random() * listaAmigos.length)];
+    console.log(sorteado);
+    document.getElementById("nomeSorteado").innerHTML = sorteado;
+    listaDeSorteados.push(sorteado);
+    listaAmigos.pop(sorteado);
+    mostrarLista();
   }
-  console.log(listaAmigos);
 }
+
+// O usuário deverá adicionar nomes por meio de um campo de texto e de um botão "Adicionar". 👌
+
+// Os nomes inseridos serão exibidos em uma lista visível na página 👌
+
+// Um botão "Sortear Amigo" selecionará um dos nomes de forma aleatória, exibindo o resultado na tela. 👌
+
+// Funcionalidades:
+// Adicionar nomes: Os usuários escreverão o nome de um amigo em um campo de texto e o adicionarão a uma lista visível ao clicar em "Adicionar". 👌
+
+// Validar entrada: Se o campo de texto estiver vazio, o programa exibirá um alerta solicitando um nome válido. 👌
+
+// Visualizar a lista: Os nomes inseridos aparecerão em uma lista abaixo do campo de entrada. 👌
+
+// Sorteio aleatório: Ao clicar no botão "Sortear Amigo", um nome da lista será selecionado aleatoriamente e exibido na página. 👌
