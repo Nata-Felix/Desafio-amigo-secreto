554612a8921eb680f390254f33aa0472b46db4fd
Natã Félix <Nata.analista@gmail.com>
2025-02-03 00:39:30 -0300

    modificação de layout

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let listaAmigos = [];
 let listaDeSorteados = [];
 
 function mostrarLista() {
-  document.getElementById("listaAmigos").innerHTML = listaAmigos;
+  document.getElementById("nome-participante").innerHTML = listaAmigos;
 }
 
 function adicionarAmigo() {
@@ -13,6 +13,25 @@ function adicionarAmigo() {
     listaAmigos.push(amigo);
     mostrarLista();
   }
+}
+
+function fogosArtificio() {
+  campoNomeSorteado = document.getElementById("nomeSorteado").value;
+  if (campoNomeSorteado !== "") {
+    document
+      .getElementById("secao-sorteado__botao")
+      .addEventListener("click", function () {
+        for (let i = 0; i < 5; i++) {
+          setTimeout(() => {
+            confetti({
+              particleCount: 100,
+              spread: 70,
+              origin: { x: Math.random(), y: Math.random() * 0.6 },
+            });
+          }, i * 500);
+        }
+      });
+  } // Adicionando a chave de fechamento aqui
 }
 
 function sortearAmigo() {
@@ -26,18 +45,3 @@ function sortearAmigo() {
     mostrarLista();
   }
 }
-
-// O usuário deverá adicionar nomes por meio de um campo de texto e de um botão "Adicionar". 👌
-
-// Os nomes inseridos serão exibidos em uma lista visível na página 👌
-
-// Um botão "Sortear Amigo" selecionará um dos nomes de forma aleatória, exibindo o resultado na tela. 👌
-
-// Funcionalidades:
-// Adicionar nomes: Os usuários escreverão o nome de um amigo em um campo de texto e o adicionarão a uma lista visível ao clicar em "Adicionar". 👌
-
-// Validar entrada: Se o campo de texto estiver vazio, o programa exibirá um alerta solicitando um nome válido. 👌
-
-// Visualizar a lista: Os nomes inseridos aparecerão em uma lista abaixo do campo de entrada. 👌
-
-// Sorteio aleatório: Ao clicar no botão "Sortear Amigo", um nome da lista será selecionado aleatoriamente e exibido na página. 👌
